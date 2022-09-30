e28bd7735793c1dcd42ff4534fb783b709eb1e6a
sarz2 <sara.abou@chasacademy.se>
2022-09-30 16:00:58 +0200

    start design for header

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import "./Homepage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default HomePage;
