b30637be85acb2742d23794419b08e6b6a65976d
sarz2 <sara.abou@chasacademy.se>
2022-10-03 14:19:56 +0200

    small design stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import "./App.css";
+import ProductPage from "./pages/Productpage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
       </Routes>
     </Router>
   );
