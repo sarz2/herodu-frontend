d9d2ff5db834329aa0e6fa51f3ca27f5555653ac
sarz2 <sara.abou@chasacademy.se>
2022-10-19 17:48:18 +0200

    create routes for cart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import "./App.css";
 import ProductPage from "./pages/Productpage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/cart/:id" element={<CartPage />} />
       </Routes>
     </Router>
   );
