4d00fa23b2094f01872eabd5d66851f38f137657
sarz2 <sara.abou@chasacademy.se>
2022-10-19 17:24:43 +0200

    create cartpage

---
+++ src/pages/CartPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const CartPage = () => {
+  return (
+    <>
+      <Header />
+      <div>Cart</div>
+      <Footer />
+    </>
+  );
+};
+
+export default CartPage;
