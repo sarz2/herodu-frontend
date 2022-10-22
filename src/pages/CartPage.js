433f914fdde37ce6b48ff7d71ddf052f9b1bf19c
sarz2 <sara.abou@chasacademy.se>
2022-10-22 14:56:39 +0200

    create first draft of cartpage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useSearchParams } from "react-router-dom";
+import { Row, Col, ListGroup, Form, Button, Card } from "react-bootstrap";
+
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 const CartPage = () => {
+  const [searchParams] = useSearchParams();
+  const productId = useParams();
+
+  const qty = searchParams.get("qty");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId.id != undefined) {
+      dispatch(addToCart(productId.id, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return (
     <>
       <Header />
