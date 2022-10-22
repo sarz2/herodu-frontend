003cae7530056b012f098ca80d17de61d097509a
sarz2 <sara.abou@chasacademy.se>
2022-10-22 15:40:11 +0200

    create redux for signup

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,6 +2,10 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_SIGNUP_FAIL,
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -32,6 +36,46 @@ export const login = (email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
+};
+
+export const signup = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_SIGNUP_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/signup",
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_SIGNUP_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_SIGNUP_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_SIGNUP_FAIL,
+  USER_SIGNUP_REQUEST,
+  USER_SIGNUP_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -15,6 +18,19 @@ export const userLoginReducer = (state = {}, action) => {
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userSignupReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_SIGNUP_REQUEST:
+      return { loading: true };
+    case USER_SIGNUP_SUCCESS:
+      return { loading: false, userInfo: action.payload };
+    case USER_SIGNUP_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
