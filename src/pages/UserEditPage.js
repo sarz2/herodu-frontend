03c44bcc951383ae7c5b2689abefa02c5f6a932d
sarz2 <sara.abou@chasacademy.se>
2022-10-27 19:15:27 +0200

    admin user edit page

---
+++ src/pages/UserEditPage.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import FormContainer from "../components/FormContainer";
+import { getUserDetails } from "../actions/userActions";
+
+const UserEditPage = ({}) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  useEffect(() => {});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Header />
+      <Link to="/admin/userlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Sign Up</h1>
+        {error && <Message variant="danger">{error}</Message>}
+        {loading && <Loader />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Update
+          </Button>
+        </Form>
+      </FormContainer>
+      <Footer />
+    </>
+  );
+};
+
+export default UserEditPage;
