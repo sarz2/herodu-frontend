5b170805c956c6370eeb5eb8a07aadd2e8f2d958
sarz2 <sara.abou@chasacademy.se>
2022-11-07 12:55:42 +0100

    Add pagination on Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,17 +8,22 @@ import Message from "../components/Message";
 import Product from "../components/Product";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Paginate from "../components/Paginate";
 import "./Homepage.css";
+import { useParams } from "react-router-dom";
 
 const HomePage = () => {
+  const { pageNumber } = useParams() || 1;
+  const { keyword } = useParams();
+
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, page, pages } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
@@ -29,14 +34,21 @@ const HomePage = () => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row className="homepage-container">
-          {" "}
-          {products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
+        <>
+          <Row className="homepage-container">
+            {" "}
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
+        </>
       )}
 
       <Footer />
