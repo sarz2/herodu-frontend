657c156c95ea84c235395d61f85c67fd2cfaa519
sarz2 <sara.abou@chasacademy.se>
2022-11-07 11:50:45 +0100

    Change color of stars from black to gold

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -6,43 +6,43 @@ const Rating = ({ value, text }) => {
     <div className="rating">
       <span>
         {value && value >= 1 ? (
-          <FaStar />
+          <FaStar style={{ color: "gold" }} />
         ) : value >= 0.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={{ color: "gold" }} />
         ) : (
           <FaRegStar />
         )}
       </span>
       <span>
         {value && value >= 2 ? (
-          <FaStar />
+          <FaStar style={{ color: "gold" }} />
         ) : value >= 1.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={{ color: "gold" }} />
         ) : (
           <FaRegStar />
         )}
       </span>
       <span>
         {value && value >= 3 ? (
-          <FaStar />
+          <FaStar style={{ color: "gold" }} />
         ) : value >= 2.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={{ color: "gold" }} />
         ) : (
           <FaRegStar />
         )}
       </span>
       <span>
         {value && value >= 4 ? (
-          <FaStar />
+          <FaStar style={{ color: "gold" }} />
         ) : value >= 3.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={{ color: "gold" }} />
         ) : (
           <FaRegStar />
         )}
       </span>
       <span>
         {value && value >= 5 ? (
-          <FaStar />
+          <FaStar style={{ color: "gold" }} />
         ) : value >= 4.5 ? (
           <FaStarHalfAlt />
         ) : (
