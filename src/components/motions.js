efbed10600c833188290e62a0ab7eea5e8f20d6b
sarz2 <sara.abou@chasacademy.se>
2022-11-10 22:03:08 +0100

    Create motion.js file for testing out animation

---
+++ src/components/motions.js
@@ -0,0 +1,16 @@
+const pageVariants = {
+  in: {
+    opacity: 1,
+    x: 0,
+  },
+  out: {
+    opacity: 0,
+    x: "-100vw",
+  },
+};
+
+const pageTransition = {
+  duration: 1,
+};
+
+export { pageVariants, pageTransition };
