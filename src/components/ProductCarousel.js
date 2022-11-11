df41d458ddcebd0007734c7369917a4c4e94f851
sarz2 <sara.abou@chasacademy.se>
2022-11-11 20:39:58 +0100

    Change design on carousel

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -21,16 +21,12 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel pause="hover" className="bg-dark">
+    <Carousel pause="hover" className="bg-primary">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`product/${product._id}`}>
-            <Image
-              className="carousel"
-              src={product.image}
-              alt={product.name}
-            />
-            <Carousel.Caption className="carousel-caption">
+            <Image src={product.image} alt={product.name} />
+            <Carousel.Caption>
               <h2>
                 {product.name}, {product.price}kr{" "}
               </h2>
