35b7d065c9c49fb08548ce71199fa5d5960d5ffc
sarz2 <sara.abou@chasacademy.se>
2022-11-13 19:29:26 +0100

    dispatch for reducing qty in product

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import {
@@ -10,24 +10,40 @@ import {
   ListGroup,
   Image,
 } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./CheckoutPage.css";
 import { Link, useNavigate } from "react-router-dom";
+import { listProducts, reduceProducts } from "../redux/actions/productActions";
 
 const CheckoutPage = () => {
   const [purchased, setPurchased] = useState(false);
   const [adress, setAdress] = useState("");
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+
+  const productList = useSelector((state) => state.productList);
+  const { products } = productList;
 
   const submitHandler = () => {
     setPurchased(!purchased);
   };
 
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  const findItemInProductList = () => {
+    cartItems.forEach((item) => {
+      dispatch(reduceProducts(item.product, item.qty));
+    });
+  };
+
   const removeHandler = () => {
+    alert("They are on their way!");
     localStorage.removeItem("cartItems");
     navigate("/");
   };
@@ -35,14 +51,20 @@ const CheckoutPage = () => {
     <>
       <Header />
       {!purchased ? (
-        <Row>
-          <Link to="/cart" className="btn btn-light my-3">
-            Go Back
-          </Link>
+        <Row style={{ paddingBottom: "12rem" }}>
           <Col>
+            <Link to="/cart" className="go-back-link">
+              Go Back
+            </Link>
             <h1>Pay with invoice</h1>
             <Form
-              style={{ width: "50%", margin: "auto" }}
+              style={{
+                width: "50%",
+                margin: "auto",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
               onSubmit={submitHandler}
             >
               <Form.Group controlId="adress">
@@ -84,9 +106,9 @@ const CheckoutPage = () => {
                 .toFixed(2)}
               kr
             </strong>
-            <Button type="button" onClick={removeHandler}>
+            <button className="btn" type="button" onClick={removeHandler}>
               Yes, send it to me!
-            </Button>
+            </button>
           </div>
         </>
       )}
